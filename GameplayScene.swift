9770f32bca81ad855ab5dcfdbcc3b65bfa69d77d
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-01-09 14:37:47 -0500

    Setting Up Controllers

---
+++ GameplayScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameplayScene.swift
+//  FlappyBirdClone
+//
+//  Created by Deborah on 1/9/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
