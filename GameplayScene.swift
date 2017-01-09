5f3dfeeeaf6fe9ad84e5bf5e831ffb2d5555485c
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-01-09 14:51:25 -0500

    Adding Backgrounds and Grounds

--- GameplayScene.swift
+++ GameplayScene.swift
@@ -11,6 +11,34 @@ import SpriteKit
 class GameplayScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        initalize();
+    }
+    
+    func initalize() {
+        createBackgrounds();
+        createGrounds();
+    }
+    
+    func createBackgrounds() {
+        for i in 0...2 {
+        let bg = SKSpriteNode(imageNamed: "BG Day");
+        bg.name = "BG";
+        bg.zPosition = 0;
+        bg.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+        bg.position = CGPoint(x: CGFloat(i) * bg.size.width, y: 0);
+        self.addChild(bg);
+            
+        }
+    }
+    
+    func createGrounds() {
+        for i in 0...2 {
+        let ground = SKSpriteNode(imageNamed: "Ground");
+        ground.name = "Ground";
+        ground.zPosition = 4;
+        ground.anchorPoint = CGPoint(x: 0.5, y: 0.5);
+        ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: -(self.frame.size.height / 2));
+        self.addChild(ground);
+        }
     }
 }
